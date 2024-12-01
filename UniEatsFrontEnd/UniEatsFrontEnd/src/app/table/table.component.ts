fe57b01b4a5cf2ce01a99a45bb3252ccb488bc6a
mmsa1 <mmsa155j3@example.com>
2024-12-01 13:29:49 +0500

    ReservationDone

---
+++ UniEatsFrontEnd/UniEatsFrontEnd/src/app/table/table.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css'
+})
+export class TableComponent {
+  @Input() tableNumber: number = 0;  // Table number (1, 2, 3, ...)
+  @Input() status: 'selected' | 'available' | 'occupied' = 'available';  // Table status
+  @Output() tableClick = new EventEmitter<number>();  // Event emitter for table click
+
+  onTableClick() {
+    this.tableClick.emit(this.tableNumber);  // Emit the selected table number
+  }
+}
