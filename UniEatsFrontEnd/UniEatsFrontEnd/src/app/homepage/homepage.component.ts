bd0be7f8da2509dcffcb6f0d311f11cc99a521fc
mmsa1 <mmsa155j3@example.com>
2024-12-01 14:25:49 +0500

    Favicon

--- UniEatsFrontEnd/UniEatsFrontEnd/src/app/homepage/homepage.component.ts
+++ UniEatsFrontEnd/UniEatsFrontEnd/src/app/homepage/homepage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { Router } from 'express';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RouterModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
