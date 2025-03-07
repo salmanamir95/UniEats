99cca47c997785711ad5e737c2e1509a28a34894
mmsa1 <mmsa155j3@example.com>
2025-03-07 12:55:35 +0500

    Login Page

--- UniEatsFrontend/src/app/Login/login/login.component.ts
+++ UniEatsFrontend/src/app/Login/login/login.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Logging in with', this.loginForm.value);
+    } else {
+      throw new Error('Form is invalid.');
+    }
+  }
+
 
 }
