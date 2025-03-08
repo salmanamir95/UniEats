7794010cfaf904733b716c32917d6404e8fe3b5a
mmsa1 <mmsa155j3@example.com>
2025-03-08 08:04:51 +0500

    All pages done now Logic

---
+++ UniEatsFrontend/src/app/Student/check-balance/check-balance.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-balance',
+  imports: [],
+  templateUrl: './check-balance.component.html',
+  styleUrl: './check-balance.component.css'
+})
+export class CheckBalanceComponent {
+
+}
---
+++ UniEatsFrontend/src/app/Student/order-food/order-food.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-food',
+  imports: [],
+  templateUrl: './order-food.component.html',
+  styleUrl: './order-food.component.css'
+})
+export class OrderFoodComponent {
+
+}
