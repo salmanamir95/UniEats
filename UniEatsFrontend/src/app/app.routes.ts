7bd20127a35c10ddcfde694ecf883b2d0dcc9434
mmsa1 <mmsa155j3@example.com>
2025-03-08 07:09:11 +0500

    Routes

--- UniEatsFrontend/src/app/app.routes.ts
+++ UniEatsFrontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Login/login/login.component';
 import { RegisterComponent } from './Register/register/register.component';
+import { StudentComponent } from './Student/student/student.component';
+import { EmployeeComponent } from './Employee/employee/employee.component';
+import { AdminComponent } from './Admin/admin/admin.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirect empty path to login
-    { path: 'login', component: LoginComponent } ,
-    {path: 'register', component: RegisterComponent}
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'student', component: StudentComponent },
+    { path: 'employee', component: EmployeeComponent },
+    { path: 'admin', component: AdminComponent }
 ];
