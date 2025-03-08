8f3cb9c05beb23a9e72901eb125503d4e44e3c3b
mmsa1 <mmsa155j3@example.com>
2025-03-08 07:56:36 +0500

    Employee Completed Pages

---
+++ UniEatsFrontend/src/app/Employee/update-menu/update-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-menu',
+  imports: [],
+  templateUrl: './update-menu.component.html',
+  styleUrl: './update-menu.component.css'
+})
+export class UpdateMenuComponent {
+
+}
---
+++ UniEatsFrontend/src/app/Employee/view-reports/view-reports.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-reports',
+  imports: [],
+  templateUrl: './view-reports.component.html',
+  styleUrl: './view-reports.component.css'
+})
+export class ViewReportsComponent {
+
+}
